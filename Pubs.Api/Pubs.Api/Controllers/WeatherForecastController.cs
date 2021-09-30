dc32cd2d4c95936007856751151379e13df6869a
Daniel Mrózek <daniel.mrozek@notino.com>
2021-09-30 16:13:41 +0200

    xxx

--- Pubs.Api/Pubs.Api/Controllers/WeatherForecastController.cs
+++ Pubs.Api/Pubs.Api/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace Pubs.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", 
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
